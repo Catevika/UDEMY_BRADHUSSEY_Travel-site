e4c2a7fd504e51ffcc44b33734c6a603d0773412
Catevika <dominique.bello@outlook.fr>
2021-02-10 15:58:10 +0100

    Start secret area and deploy on Netlify

---
+++ cloud-functions/secret-area.js
@@ -0,0 +1,26 @@
+const myPassword = process.env.PASSWORD;
+
+exports.handler = function (event, context, callback) {
+	const secretContent = `
+		<h3>Welcome to the secret area</h3>
+		<p>Here we can tell you that the sky is <strong>blue</strong> and two plus two equals four.</p>
+	`;
+
+	let body;
+	if (event.body) {
+		body = JSON.parse(event.body);
+	} else {
+		body = {};
+	}
+
+	if (body.password == myPassword) {
+		callback(null, {
+			statusCode: 200,
+			body: secretContent
+		});
+	} else {
+		callback(null, {
+			statusCode: 401
+		});
+	}
+};
